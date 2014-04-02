b286560e0940f565dce211b586cb53615f4b2dbc
Cedric Thiebault <cedric.thiebault@gmail.com>
2014-04-01 22:05:20 -0400

    Add logback.xml

--- src/main/java/com/github/cthiebault/Application.java
+++ src/main/java/com/github/cthiebault/Application.java
@@ -2,7 +2,9 @@ package com.github.cthiebault;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @EnableAutoConfiguration
 public class Application {
 
